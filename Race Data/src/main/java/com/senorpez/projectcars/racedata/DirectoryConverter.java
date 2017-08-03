8067a2616808ab2ff24d0dda85cf7404a9826032
SenorPez <senorpez@senorpez.com>
2017-08-03 13:12:50 -0500

    Move directory conversion utility. Quick and dirty for now.

---
+++ Race Data/src/main/java/com/senorpez/projectcars/racedata/DirectoryConverter.java
@@ -0,0 +1,44 @@
+package com.senorpez.projectcars.racedata;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.*;
+
+public class DirectoryConverter {
+    public static void main(final String[] args) {
+        assert args.length == 2;
+
+        final Path telemetryDirectory = Paths.get(args[0]);
+        final Path outputFile = Paths.get(args[1]);
+
+        try {
+            Files.write(outputFile, new byte[]{}, CREATE, TRUNCATE_EXISTING);
+            Files.list(telemetryDirectory)
+                    .filter(path -> !path.toFile().getName().replaceAll("[^\\d]", "").equals(""))
+                    .sorted((o1, o2) -> {
+                        Integer n1 = Integer.valueOf(o1.toFile().getName().replaceAll("[^\\d]", ""));
+                        Integer n2 = Integer.valueOf(o2.toFile().getName().replaceAll("[^\\d]", ""));
+                        return n1.compareTo(n2);
+                    })
+                    .forEach(path -> {
+                        try {
+                            final byte[] fileData = Files.readAllBytes(path);
+                            System.out.println(path.toString());
+
+                            final ByteBuffer buffer = ByteBuffer.allocate(2);
+                            buffer.putShort((short) fileData.length);
+                            Files.write(outputFile, buffer.array(), APPEND);
+                            Files.write(outputFile, fileData, APPEND);
+                        } catch (final IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- Race Data/src/main/java/com/senorpez/projectcars/racedata/Telemetry.java
+++ Race Data/src/main/java/com/senorpez/projectcars/racedata/Telemetry.java
@@ -1,13 +1,8 @@
 package com.senorpez.projectcars.racedata;
 
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
-
-import static java.nio.file.StandardOpenOption.*;
+import java.util.Iterator;
 
 public class Telemetry implements Iterator<Packet> {
     private final DataInputStream telemetryData;
@@ -38,34 +33,6 @@ public class Telemetry implements Iterator<Packet> {
             return packetType.getPacket(telemetryData);
         } catch (IOException e) {
             return null;
-        }
-    }
-
-    public static void convertDirectory(Path telemetryDirectory, Path outputFile) {
-        try {
-            Files.write(outputFile, new byte[]{}, CREATE, TRUNCATE_EXISTING);
-            Files.list(telemetryDirectory)
-                    .filter(path -> !path.toFile().getName().replaceAll("[^\\d]", "").equals(""))
-                    .sorted((o1, o2) -> {
-                        Integer n1 = Integer.valueOf(o1.toFile().getName().replaceAll("[^\\d]", ""));
-                        Integer n2 = Integer.valueOf(o2.toFile().getName().replaceAll("[^\\d]", ""));
-                        return n1.compareTo(n2);
-                    })
-                    .forEach(path -> {
-                        try {
-                            byte[] fileData = Files.readAllBytes(path);
-                            System.out.println(path.toString());
-
-                            ByteBuffer buffer = ByteBuffer.allocate(2);
-                            buffer.putShort((short) fileData.length);
-                            Files.write(outputFile, buffer.array(), APPEND);
-                            Files.write(outputFile, fileData, APPEND);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
