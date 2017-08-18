05e029495a6c6dee7fe8917e47b3db6ef779d072
SenorPez <senorpez@senorpez.com>
2017-08-18 16:33:03 -0500

    Pass TestRoot_InvalidAcceptHeader test.

--- static-data-api/src/main/java/com/senorpez/projectcars/staticdata/APIExceptionHandler.java
+++ static-data-api/src/main/java/com/senorpez/projectcars/staticdata/APIExceptionHandler.java
@@ -1,18 +1,16 @@
-//package com.senorpez.projectcars.staticdata;
-//
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.HttpMediaTypeNotAcceptableException;
-//import org.springframework.web.HttpRequestMethodNotSupportedException;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.ResponseStatus;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//import org.springframework.web.servlet.NoHandlerFoundException;
-//
-//import static org.springframework.http.HttpStatus.*;
-//
-//@RestControllerAdvice
-//public class APIExceptionHandler {
+package com.senorpez.projectcars.staticdata;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
+
+@RestControllerAdvice
+public class APIExceptionHandler {
 //    @ResponseStatus(NOT_FOUND)
 //    @ExceptionHandler(NoHandlerFoundException.class)
 //    ErrorResponse handle404NotFound() {
@@ -25,21 +23,21 @@
 //        return new ErrorResponse(METHOD_NOT_ALLOWED);
 //    }
 //
-//    @ResponseStatus(NOT_ACCEPTABLE)
-//    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
-//    ErrorResponse handle406NotAcceptable() {
-//        return new ErrorResponse(NOT_ACCEPTABLE);
-//    }
-//
-//    private class ErrorResponse {
-//        @JsonProperty("code")
-//        private final int code;
-//        @JsonProperty("message")
-//        private final String message;
-//
-//        private ErrorResponse(final HttpStatus httpStatus) {
-//            this.code = httpStatus.value();
-//            this.message = httpStatus.getReasonPhrase();
-//        }
-//    }
-//}
+    @ResponseStatus(NOT_ACCEPTABLE)
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    ErrorResponse handle406NotAcceptable() {
+        return new ErrorResponse(NOT_ACCEPTABLE);
+    }
+
+    private class ErrorResponse {
+        @JsonProperty("code")
+        private final int code;
+        @JsonProperty("message")
+        private final String message;
+
+        private ErrorResponse(final HttpStatus httpStatus) {
+            this.code = httpStatus.value();
+            this.message = httpStatus.getReasonPhrase();
+        }
+    }
+}
