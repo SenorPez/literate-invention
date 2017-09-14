2ba93cb2e5603f2e4766b231fd0509b0a8fd0a02
SenorPez <senorpez@senorpez.com>
2017-09-14 09:33:51 -0500

    Cleanup for Gradle project.

--- race-report-old/src/test/java/com/senorpez/projectcars/racereport/RootControllerTest.java
+++
@@ -1,9 +0,0 @@
-package com.senorpez.projectcars.racereport;
-
-import org.springframework.http.MediaType;
-
-class RootControllerTest {
-    static final MediaType MEDIA_TYPE = MediaType.APPLICATION_JSON_UTF8;
-    static final ClassLoader CLASS_LOADER = RootControllerTest.class.getClassLoader();
-    static final String OBJECT_SCHEMA = "root.schema.json";
-}
